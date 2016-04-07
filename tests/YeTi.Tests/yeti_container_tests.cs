8dfcfed0416db1a386be6d886b757b2c4a7c9aaf
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2016-04-07 13:43:06 +0200

    removed comments in tests file

--- tests/YeTi.Tests/yeti_container_tests.cs
+++ tests/YeTi.Tests/yeti_container_tests.cs
@@ -8,14 +8,11 @@ namespace YeTi.Tests
         [Fact]
         public void resolves_registered_components()
         {
-            // Arrange
             var container = new YeTiContainer();
             container.Register<ITestInterface, TestImplementation>();
 
-            // Act
             var resolved_object = container.Resolve<ITestInterface>();
 
-            // Arrange
             resolved_object.ShouldBeOfType<TestImplementation>();
         }
 
