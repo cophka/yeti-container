83106c8a0a7b0f8b33b5fb1e4f2da4f25eae2242
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2016-04-18 22:37:03 +0200

    added support for components with dependencies

--- src/YeTi/YeTiContainer.cs
+++ src/YeTi/YeTiContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace YeTi
 {
@@ -12,15 +13,31 @@ namespace YeTi
             _registrations.Add(typeof(TRegistration), typeof(TImplementation));
         }
 
-        public T Resolve<T>()
+        object Resolve(Type type)
         {
-            var requested_type = typeof(T);
+            var requested_type = type;
 
             Type actual_type = _registrations[requested_type];
 
-            var instance = Activator.CreateInstance(actual_type);
+            var ctors = actual_type.GetConstructors();
 
-            return (T) instance;
+            var ctor = ctors.First();
+
+            IEnumerable<Type> dependency_types = ctor.GetParameters()
+                .Select(x => x.ParameterType);
+
+            var dependencies = dependency_types
+                .Select(x => this.Resolve(x))
+                .ToArray();
+
+            var instance = Activator.CreateInstance(actual_type, dependencies);
+
+            return instance;
+        }
+
+        public T Resolve<T>()
+        {
+            return (T) this.Resolve(typeof(T));
         }
     }
 }
--- tests/YeTi.Tests/yeti_container_tests.cs
+++ tests/YeTi.Tests/yeti_container_tests.cs
@@ -16,6 +16,18 @@ namespace YeTi.Tests
             resolved_object.ShouldBeOfType<TestImplementation>();
         }
 
+        [Fact]
+        public void resolves_components_with_ctor_with_dependencies()
+        {
+            var container = new YeTiContainer();
+            container.Register<Dependency, Dependency>();
+            container.Register<ITestInterface, TestImplementationWithDependency>();
+
+            var resolved_object = container.Resolve<ITestInterface>();
+
+            resolved_object.ShouldBeOfType<TestImplementationWithDependency>();
+        }
+
         public interface ITestInterface
         {
              
@@ -23,6 +35,19 @@ namespace YeTi.Tests
 
         public class TestImplementation : ITestInterface
         {
+        }
+
+        public class Dependency
+        {
+             
+        }
+
+        public class TestImplementationWithDependency : ITestInterface
+        {
+            public TestImplementationWithDependency(Dependency dependency)
+            {
+                
+            }
         }
     }
 }
